043aa41852e96242bc2bb5a240f41283b4578804
korlir <anonym.show@mail.ru>
2022-07-01 05:08:38 +0300

    Fix .gitignore

--- Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-import MatrixMath(Matrix (Matrix), mulNum, calcSize)
-main :: IO()
-main = do
-    print (m1 `mulNum` 2)
-    where
-        m1 = Matrix [[2, 3, 4], [0, 1, 2]]
-        m2 = Matrix [[2, 4, 6], [0, 0, 0], [1, 1 ,1]]
